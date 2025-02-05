5f322798e2d3bdb7b33fea7b34a06dbf8d38d539
AnyaVeselova <anna.correspondence@outlook.com>
2025-02-05 20:49:03 +0000

    adds browserSync, posts a user from the form to the list

--- web-page-project/server.js
+++ web-page-project/server.js
@@ -1,7 +1,40 @@
 const express = require("express");
 const app = express();
+const browserSync = require('browser-sync');
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+
+
+
+//Form
+// Middleware to parse JSON data from requests
+
+app.get("/form", (req, res) => {
+	res.sendFile(__dirname + "/public/form.html");
+});
+const users = [];
+app.post("/add-user", (req, res) => {
+
+	const { name, age } = req.body;
+	
+	if (!name || !age) {
+	  return res.status(400).json({ message: "Both name and age are required!" });
+	}
+
+
+  
+	const newUser = { id: users.length + 1, name, age };
+	users.push(newUser);
+  
+	res.status(201).json({ message: "User added!", user: newUser });
+  });
+  
+  
+  app.get("/users", (req, res) => {
+	res.json(users);
+  });
 
 app.use((req, res, next) => {
 	console.log(`Request: ${req.method} ${req.url}`);
@@ -27,7 +60,14 @@ app.get('/storytime', (req, res) => {
     res.sendFile(__dirname + '/public/storytime.html');
 });
 
+
+
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+browserSync.init({
+	proxy: `http://localhost:${PORT}`,
+	files: ['public/*/.*'], 
+	reloadDelay: 50,
+  });
