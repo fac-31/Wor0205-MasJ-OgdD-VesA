60c8fedca9c1ca4dffbcc7cdb37053e56a958064
AnyaVeselova <anna.correspondence@outlook.com>
2025-02-05 15:55:15 +0000

    creates annas-page and browserSync functionality

--- web-page-project/server.js
+++ web-page-project/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const browserSync = require('browser-sync');
+
 app.use((req, res, next) => {
 	console.log(`Request: ${req.method} ${req.url}`);
 	next();
@@ -16,6 +18,22 @@ app.get("/about", (req, res) => {
 app.get("/about-jaz", (req, res) => {
 	res.sendFile(__dirname + "/public/about-jaz.html");
 });
+
+app.get("/annas-page", (req, res) => {
+	res.sendFile(__dirname + "/public/annas-page.html");
+});
+
+app.get("/about/:name", (req, res) => {
+	const user = req.params.name;
+	res.sendFile(__dirname + `/public/about-${user}.html`);
+});
+
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+browserSync.init({
+	proxy: http://localhost:${PORT},
+	files: ['public/*/.*'], 
+	reloadDelay: 50,
+  });
